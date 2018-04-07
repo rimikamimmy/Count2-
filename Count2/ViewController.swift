917e563d753fd529a24237bdc2de476efc256105
RIMIKA <rimikamimmy727@gmail.com>
2018-04-07 14:58:19 +0900

    カウントアプリ作った

--- Count2/ViewController.swift
+++ Count2/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(){
+        number=number+1
+        label.text=String(number)
+    }
+    
+    @IBAction func minus(){
+        number=number-1
+        label.text=String(number)
+    }
+    
+    @IBAction func multiple(){
+        number=number*2
+        label.text=String(number)
+    }
+    
+    @IBAction func divide(){
+        number=number/2
+        label.text=String(number)
+    }
+    
+    @IBAction func clear(){
+        number=number-number
+        label.text=String(number)
+    }
 
 }
 
